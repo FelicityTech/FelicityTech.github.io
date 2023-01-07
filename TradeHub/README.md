e7229a934b427b5a8d7232a2519c62f9d9de60d4
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-01-07 23:42:17 +0100

    TradeHub where you can buy and sell

--- TradeHub/README.md
+++ TradeHub/README.md
@@ -1,3 +1,2 @@
 # TradeHub
-
-## This is Frontend website for TradeHub where you can post goods for selling, where you can acquire goods by creating account.
+## this is a buying and selling website
---
+++ TradeHub/script2.js
@@ -0,0 +1,61 @@
+// add JavaScript for the landing page here
+    // get the login and register links
+    const loginLink = document.querySelector('header .login');
+    const registerLink = document.querySelector('header .register');
+    
+    // add a click event listener to the login link
+    loginLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      // show the login form and hide the landing page
+      loginForm.style.display = 'block';
+      landingPage.style.display = 'none';
+    });
+    
+    // add a click event listener to the register link
+    registerLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      // show the register form and hide the landing page
+      registerForm.style.display = 'block';
+      landingPage.style.display = 'none';
+    })
+    
+    // get the login form
+    const loginForm = document.getElementById('login-form');
+    
+    // add a submit event listener to the login form
+    loginForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      // get the form data
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+      // validate the form data
+      if (!username || !password) {
+        alert('Please fill out all fields');
+        return;
+      }
+
+      // make a POST request to the /login endpoint
+      fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'username': username,
+          'password': password
+        })
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // show the trading page and hide the login form
+          tradingPage.style.display = 'block';
+          loginForm.style.display = 'none';
+        } else {
+          // display an error message
+          alert (data.message);
+        }
+      });
+    });
+
+  
